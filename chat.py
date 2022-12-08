bf29615326cbc51de0aa2c84a8863b033c1dded8
Daniel Yakov <daniel577000@gmail.com>
2022-12-08 22:10:47 +0200

    Switch file for Mysql as DB

--- chat.py
+++ chat.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 import datetime
+import mysql.connector
+
+massagesDB = mysql.connector.connect(
+  host='localhost',
+  user="root",
+  password="password",
+  database="messagesDB",
+  auth_plugin='mysql_native_password'
+)
+
+# Act as middlewares between app to mysql
+bufferedCursor = massagesDB.cursor(buffered=True)
+regularCursor = massagesDB.cursor()
+
+sql = bufferedCursor.execute
 
 app = Flask(__name__)
 
@@ -18,29 +33,39 @@ def render_index_room(room):
 # Load chat of specific room
 @app.get("/chat/<room>")
 def render_chat_room(room):
-    # create file if not exist
     try:
-        file = open(room, 'x')
+        sql('select * from ' + room + ';')
     except:
-        pass
+        sql('create table ' + room + ' (massageId int NOT NULL AUTO_INCREMENT key, massageContent char(255));')
     return render_template('index.html')
 
 # Handle the loading of the chat box
 @app.get("/api/chat/<room>")
 def updateChat(room):
-    file = open(room)
-    return file
+    # Fetch all the chat's massages
+    sqlquery = 'select massageContent from %s;' % room
+    regularCursor.execute(sqlquery)
+    
+    # Format the output nicly
+    chatContent = ""
+    for line in regularCursor.fetchall():
+        chatContent += line[0]
+    
+    return chatContent
 
 # Upadate the chat massages and save then to thier appropriate file
 @app.post("/api/chat/<room>")
 def sendrequest(room):
-    file = open(room, "a+")
     # Format the date as requested
     date = '[' + str(datetime.datetime.now()).split('.')[0] + ']'
     # Prepare the massage before saving to file
     massageToSave = date + ' ' + request.form['username'] + ': ' + request.form['msg'] + '\n'
-    # Save the massage to the file
-    file.write(massageToSave)
+    
+    # Insert the posted massage to the DB
+    sqlquery = 'insert into %s (massageContent) values ("%s");' % (room, massageToSave)
+    sql(sqlquery)
+    massagesDB.commit()
+
     # redirect to room chat
     return redirect(url_for('render_chat_room', room=room))
 
